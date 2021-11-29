12b2040c7d18c7cf9e3738b5dc881db854b9cf55
Craig Katsube <Omegasounddragon@Gmail.com>
2021-11-29 14:25:12 -0500

    added configuration page extension and installation detection service (#3)

---
+++ src/main/kotlin/com/github/davidyzliu/pytapycharmplugin/extensions/PythonTAConfigurable.kt
@@ -0,0 +1,41 @@
+package com.github.davidyzliu.pytapycharmplugin.extensions
+
+import com.github.davidyzliu.pytapycharmplugin.services.MyProjectService
+import com.intellij.openapi.components.service
+import com.intellij.openapi.options.BoundSearchableConfigurable
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.layout.panel
+
+/**
+ * Project Configurable using Kotlin DSL to create a new page in the Settings menu
+ *
+ * @property configService the Project service which contains the Project SDK path.
+ * @property pathFieldContent the property linked to the DSL field, delegating the setter and getter to [configService]
+ */
+class PythonTAConfigurable(_project: Project) : BoundSearchableConfigurable("PythonTA Plugin Settings", "base") {
+
+    private var configService: MyProjectService = _project.service()
+
+    var pathFieldContent: String
+        get() = configService.getPythonSDKPath()
+        set(value) {
+            configService.setPythonSDKPath(value)
+        }
+
+    override fun createPanel(): DialogPanel {
+
+        val settingsPagePanel = panel {
+            titledRow("PythonTA Plugin Configuration") {
+                row {
+                    label("PythonTA Installation Folder")
+                }
+                row {
+                    textFieldWithBrowseButton(::pathFieldContent)
+                }
+            }
+        }
+        return settingsPagePanel
+    }
+
+}
--- src/main/kotlin/com/github/davidyzliu/pytapycharmplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/davidyzliu/pytapycharmplugin/services/MyProjectService.kt
@@ -2,8 +2,31 @@ package com.github.davidyzliu.pytapycharmplugin.services
 
 import com.intellij.openapi.project.Project
 import com.github.davidyzliu.pytapycharmplugin.MyBundle
+import com.intellij.openapi.roots.ProjectRootManager
 
+/**
+ * The Project Service (singleton) used to contain and maintain the Python SDK Path selected for the plugin's use.
+ *
+ * @property selectedPythonSDKPath the Python SDK path the user has set to be used
+ * @constructor detects and uses the path of the default project SDK
+ */
 class MyProjectService(project: Project) {
+
+    private var selectedPythonSDKPath: String = ""
+
+    init {
+        val sdk = ProjectRootManager.getInstance(project).projectSdk
+        if (sdk != null)
+            selectedPythonSDKPath = sdk.homePath.toString();
+    }
+
+    fun getPythonSDKPath(): String {
+        return selectedPythonSDKPath
+    }
+
+    fun setPythonSDKPath(value: String) {
+        selectedPythonSDKPath = value
+    }
 
     init {
         println(MyBundle.message("projectService", project.name))
